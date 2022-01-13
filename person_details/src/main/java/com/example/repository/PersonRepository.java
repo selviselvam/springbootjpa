34987836e011b4ce3214bc8ae6c75d334190d757
selviselvam <selvicse04@gmail.com>
2022-01-13 20:31:55 +0530

    adding further code

--- person_details/src/main/java/com/example/repository/PersonRepository.java
+++ person_details/src/main/java/com/example/repository/PersonRepository.java
@@ -2,12 +2,14 @@ package com.example.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.entity.Person;
 
-public interface PersonRepository extends CrudRepository<Person,Integer> {
-@SuppressWarnings("unchecked")
-Person save(Person person);
+public interface PersonRepository extends CrudRepository<Person,Integer>{
+
+
 Person findByPersonId(int person_id);
+ 
 }
